89682ea3ad94c52ac54e9bcc1793d96d5da34a9f
justin-pyne <jpyne.justin@gmail.com>
2026-04-08 12:28:33 -0700

    feat: updated outputs to include ansible controller outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,12 +6,20 @@ output "bastion_public_dns" {
   value = aws_instance.bastion.public_dns
 }
 
-output "private_instance_private_ips" {
-  value = aws_instance.private_instances[*].private_ip
+output "controller_public_ip" {
+  value = aws_instance.ansible_controller.public_ip
 }
 
-output "private_instance_ids" {
-  value = aws_instance.private_instances[*].id
+output "controller_public_dns" {
+  value = aws_instance.ansible_controller.public_dns
+}
+
+output "amazon_private_ips" {
+  value = aws_instance.amazon_instances[*].private_ip
+}
+
+output "ubuntu_private_ips" {
+  value = aws_instance.ubuntu_instances[*].private_ip
 }
 
 output "vpc_id" {
